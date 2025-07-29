98a4cd920aa552ff8b6d6a3390e26ba70d062121
sentientt <17513780+sentientt@users.noreply.github.com>
2025-07-29 15:04:37 +0800

    卡拉赞：更新中文本地化，小怪标记选项

--- LittleWigs/Legion/Karazhan/Locales/zhCN.lua
+++ LittleWigs/Legion/Karazhan/Locales/zhCN.lua
@@ -1,11 +1,15 @@
 local L = BigWigs:NewBossLocale("Karazhan Trash", "zhCN")
 if not L then return end
 if L then
+	L.confluence = "魔力聚合体"
+	L.charger = "鬼灵战马"
 	L.skeletalUsher = "骷髅招待员"
+	L.spectral = "鬼灵家仆"
 	L.custom_on_autotalk = "自动对话"
 	L.custom_on_autotalk_desc = "立即选择巴内斯对话选项开始歌剧院战斗。"
 	L.attendant = "鬼魅随从"
 	L.hostess = "保守的女招待"
+	L.SpectralStableHand = "鬼灵马夫"
 	L.opera_hall_westfall_story_text = "歌剧院：西部故事"
 	L.opera_hall_westfall_story_trigger = "我们将认识一对分属哨兵岭敌对双方的有情人" -- 今天……我们将认识一对分属哨兵岭敌对双方的有情人。
 	L.opera_hall_beautiful_beast_story_text = "歌剧院：美女与野兽"
@@ -26,4 +30,28 @@ end
 L = BigWigs:NewBossLocale("Attumen the Huntsman", "zhCN")
 if L then
 	L.IntangiblePresence = "无形"
+end
+
+L = BigWigs:NewBossLocale("=> AutoMarks <=   ", "zhCN")
+if L then
+	L.custom_on_Allowmarks = "标记选定小怪"
+	L.custom_on_Allowmarks_desc = "用 {rt1}{rt2}{rt3}{rt4}{rt5}{rt6}{rt7} 标记小怪，你需要是助理或领袖"
+	L.custom_off_RequireLead = "仅由我（领袖）操作"
+	L.custom_off_CombatMarking  = "仅标记战斗中的小怪"
+	
+	L.Choose = "请选择需要标记的小怪"
+	L.Lower = "卡拉赞下层"
+	
+	L.custom_off_Mob1 = "尖啸女妖"
+	L.custom_off_Mob2 = "魔力聚合体"
+	L.custom_off_Mob3 = "易爆能量"
+	L.custom_off_Mob4 = "守护者的影像"
+	L.custom_off_Mob5 = "魔蝠"
+	
+	L.custom_off_Mob6 = "鬼灵家仆"
+	L.custom_off_Mob7 = "保守的女招待"
+	L.custom_off_Mob8 = "鬼灵马夫"
+	L.custom_off_Mob9 = "奥术守卫"
+	L.custom_off_Mob10 = "骷髅服务员"
+	L.custom_off_Mob11 = "骷髅猎犬"
 end
