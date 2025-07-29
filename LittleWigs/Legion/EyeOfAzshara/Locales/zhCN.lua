45ebea3abd6af42aa732e6c64843f1f88840a879
sentientt <17513780+sentientt@users.noreply.github.com>
2025-07-29 12:41:21 +0800

    艾萨拉之眼：更新中文本地化，小怪标记选项

--- LittleWigs/Legion/EyeOfAzshara/Locales/zhCN.lua
+++ LittleWigs/Legion/EyeOfAzshara/Locales/zhCN.lua
@@ -1,6 +1,7 @@
 local L = BigWigs:NewBossLocale("Eye of Azshara Trash", "zhCN")
 if not L then return end
 if L then
+	L.gritslime = "砂泥蜗牛
 	L.wrangler = "积怨牧鱼者"
 	L.stormweaver = "积怨织雷者"
 	L.crusher = "积怨碾压者"
@@ -17,4 +18,20 @@ if L then
 
 	L.water_safe = "%s（水中安全）"
 	L.land_safe = "%s（沙丘安全）"
+end
+
+
+L = BigWigs:NewBossLocale("=> AutoMarks <=", "zhCN")
+if L then
+	L.custom_on_Allowmarks = "标记选定小怪"
+	L.custom_on_Allowmarks_desc = "用 {rt1}{rt2}{rt3}{rt4}{rt5}{rt6}{rt7} 标记小怪，你需要是助理或领袖"
+	L.custom_off_RequireLead = "仅由我（领袖）操作"
+	L.custom_off_CombatMarking  = "仅标记战斗中的小怪"
+	
+	L.Choose = "请选择需要标记的小怪"
+	
+	L.custom_off_HatecoilStormweaver = "积怨织雷者"
+	L.custom_off_HatecoilArcanist = "积怨奥术师"
+	L.custom_off_MakranaHardshell = "玛拉纳硬壳战士"
+	L.custom_off_Ritualmobs = "尾王附近的仪式小怪"
 end
