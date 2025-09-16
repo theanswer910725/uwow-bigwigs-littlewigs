77c5fec20188f87c34261561d9af9bfbb76cb81d
sentientt <17513780+sentientt@users.noreply.github.com>
2025-09-16 14:56:06 +0800

    Import LittleWigs v40 base version

--- LittleWigs/LittleWigs Original/TBC/AuchenaiCrypts/Options/Colors.lua
+++
@@ -1,9 +0,0 @@
-
-BigWigs:AddColors("Shirrak the Dead Watcher", {
-	[-5041] = {"Attention","Personal"},
-})
-
-BigWigs:AddColors("Exarch Maladaar", {
-	[32346] = {"Personal","Urgent"},
-	["avatar"] = {"Attention","Important"},
-})
